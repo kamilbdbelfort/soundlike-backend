123e6df3a69ce57b8eec497010a7d951ed170f29
kamilbdbelfort <k.beelidebelfort@hotmail.com>
2021-10-21 12:05:51 +0200

    updated category route

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const app = express();
 
 // Define routers
 const SoundRouter = require("./routers/soundRouter");
+const CategoryRouter = require("./routers/soundRouter");
 
 // Use routers
 app.use("/", SoundRouter);
+app.use("/categories", CategoryRouter);
 
 // Listen for connections on specified port (default is port 4000)
 const { PORT } = require("./config/constants");
--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -3,4 +3,16 @@
 const { Router } = require("express");
 const router = new Router();
 
+const Category = require("../models").category;
+
+// GET all category names
+router.get("/", async (req, res, next) => {
+  try {
+    const nameCategories = await Category.findAll();
+    res.status(200).send(nameCategories);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
 module.exports = router;
